8ba8f4c44309290fd6678f557a9185e4b5df85e8
Steven Gerdes <steven.gerdes@tackmobile.com>
2020-07-22 09:26:57 -0500

    first commit

---
+++ app/src/main/java/com/example/shortcuts/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.shortcuts
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        toast_button.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Toast, Toast, Toast",
+                Toast.LENGTH_SHORT
+            ).show() }
+    }
+
+
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
